bb437fd2cf369e73f82f8e3d4ff773ff0034c69f
Aakash Chaudhary <akashchaudhary12121@gmail.com>
2024-10-06 16:44:42 +0530

    feature: message quque added with subscribe and publish methods

--- src/main/java/message_queue/services/MessageQueue.java
+++ src/main/java/message_queue/services/MessageQueue.java
@@ -5,7 +5,10 @@ import java.util.Map;
 import java.util.UUID;
 
 import lombok.NonNull;
+import message_queue.models.Message;
 import message_queue.models.Topic;
+import message_queue.models.TopicSubscriber;
+import message_queue.subscribers.Subscriber;
 
 public class MessageQueue {
   private final Map<String, TopicService> topicProcessors;
@@ -20,5 +23,16 @@ public class MessageQueue {
     topicProcessors.put(topic.getTopicId(), service);
     System.out.println("Created topic: " + topic.getTopicName());
     return topic;
+  }
+
+  public void subscribe(@NonNull final Subscriber subscriber, @NonNull final Topic topic) {
+    topic.addSubscriber(new TopicSubscriber(subscriber));
+    System.out.println(subscriber.getId() + " subscribed to topic: " + topic.getTopicName());
+  }
+
+  public void publish(@NonNull final Topic topic, @NonNull final Message message) {
+    topic.addMessage(message);
+    System.out.println(message.getMessageString() + " published to topic: " + topic.getTopicName());
+    new Thread(() -> topicProcessors.get(topic.getTopicId()).publish()).start();
   }
 }
